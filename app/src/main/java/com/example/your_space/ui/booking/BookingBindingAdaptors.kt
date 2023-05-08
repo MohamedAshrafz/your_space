95c0ef0b00527047555804b7f65c92d32ada5416
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2023-05-08 20:25:34 +0300

    using fab instead of context menu

--- app/src/main/java/com/example/your_space/ui/booking/BookingBindingAdaptors.kt
+++ app/src/main/java/com/example/your_space/ui/booking/BookingBindingAdaptors.kt
@@ -1,8 +1,11 @@
 package com.example.your_space.ui.booking
 
 import android.app.AlertDialog
+import android.graphics.drawable.Icon
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -11,6 +14,10 @@ import com.example.your_space.R
 import com.example.your_space.database.BookingDB
 import com.example.your_space.databinding.BookItemBinding
 
+enum class ButtonType(val asString: String) {
+    CANCEL("Cancel"),
+    DELETE("Delete")
+}
 
 class BookingRecyclerViewAdaptor(
     private val clickListener: (bookItem: BookingDB) -> Unit,
@@ -22,6 +29,7 @@ class BookingRecyclerViewAdaptor(
         return BookItemViewHolder.from(parent)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onBindViewHolder(holder: BookItemViewHolder, position: Int) {
         val item = getItem(position)
         holder.bind(item, clickListener, type)
@@ -29,25 +37,53 @@ class BookingRecyclerViewAdaptor(
 
     class BookItemViewHolder(private val binding: BookItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        @RequiresApi(Build.VERSION_CODES.M)
         fun bind(item: BookingDB, clickListener: (bookItem: BookingDB) -> Unit, type: String) {
-            binding.funcButton.text = type
+//            binding.funcButton.text = type
+
+            if (type == ButtonType.CANCEL.asString) {
+                binding.funcButton.setImageIcon(
+                    Icon.createWithResource(
+                        binding.root.context,
+                        R.drawable.ic_baseline_cancel_24
+                    )
+                )
+            } else {
+                binding.funcButton.setImageIcon(
+                    Icon.createWithResource(
+                        binding.root.context,
+                        R.drawable.ic_baseline_delete_24
+                    )
+                )
+            }
             binding.bookItem = item
-
-            binding.constraintLayoutBookItem.setOnCreateContextMenuListener { menu, _, _ ->
-
-                menu.add(type).setTitle(type).setOnMenuItemClickListener {
-                    AlertDialog.Builder(binding.root.context).apply {
-                        setTitle(R.string.warning_string)
-                        setMessage("Do you want to $type this booking?")
-                        setPositiveButton(R.string.yes_string) { _, _ ->
-                            clickListener(item)
-                        }
-                        setNegativeButton(R.string.no_string) { dialog, _ -> dialog.cancel() }
-                        show()
+            binding.funcButton.setOnClickListener {
+                AlertDialog.Builder(binding.root.context).apply {
+                    setTitle(R.string.warning_string)
+                    setMessage("Do you want to $type this booking?")
+                    setPositiveButton(R.string.yes_string) { _, _ ->
+                        clickListener(item)
                     }
-                    return@setOnMenuItemClickListener true
+                    setNegativeButton(R.string.no_string) { dialog, _ -> dialog.cancel() }
+                    show()
                 }
             }
+
+//            binding.constraintLayoutBookItem.setOnCreateContextMenuListener { menu, _, _ ->
+//
+//                menu.add(type).setTitle(type).setOnMenuItemClickListener {
+//                    AlertDialog.Builder(binding.root.context).apply {
+//                        setTitle(R.string.warning_string)
+//                        setMessage("Do you want to $type this booking?")
+//                        setPositiveButton(R.string.yes_string) { _, _ ->
+//                            clickListener(item)
+//                        }
+//                        setNegativeButton(R.string.no_string) { dialog, _ -> dialog.cancel() }
+//                        show()
+//                    }
+//                    return@setOnMenuItemClickListener true
+//                }
+//            }
 
             binding.executePendingBindings()
         }
