2ed16248a8ffc2db761f0805f80b096c09bdc404
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2024-09-24 19:19:22 +0300

    [Timber] Enhance the timber logging and its hyperlinking

--- app/src/main/java/com/example/your_space/utils/LogCatTree.kt
+++ app/src/main/java/com/example/your_space/utils/LogCatTree.kt
@@ -1,29 +1,50 @@
 package com.example.your_space.utils
 
+import android.annotation.SuppressLint
 import android.util.Log
+import com.example.your_space.BuildConfig
 import timber.log.Timber
 
 
 class LogCatTree : Timber.Tree() {
+
+    companion object {
+        private const val STACK_TRACE_LEVEL = 4
+        private const val MAX_NUMBER_OF_CALLERS = 3
+    }
+
+    @SuppressLint("LogNotTimber")
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         val caller = getCallerInfo(Throwable().stackTrace)
         when (priority) {
-            Log.VERBOSE -> Log.v(tag, "$message[SOURCE $caller]")
-            Log.DEBUG -> Log.d(tag, "$message[SOURCE $caller]")
-            Log.INFO -> Log.i(tag, "$message[SOURCE $caller]")
-            Log.WARN -> Log.w(tag, "$message[SOURCE $caller]")
-            Log.ERROR -> Log.e(tag, "$message[SOURCE $caller]")
+            Log.VERBOSE -> Log.v(tag, "$message  [SOURCE $caller]")
+            Log.DEBUG -> Log.d(tag, "$message  [SOURCE $caller]")
+            Log.INFO -> Log.i(tag, "$message  [SOURCE $caller]")
+            Log.WARN -> Log.w(tag, "$message  [SOURCE $caller]")
+            Log.ERROR -> Log.e(tag, "$message  [SOURCE $caller]")
             else -> {}
         }
     }
 
-    companion object {
-        private fun getCallerInfo(stacks: Array<StackTraceElement>?): String {
-            if (stacks == null || stacks.size < 5) {
-                return ""
-            }
-            val stack = stacks[4]
-            return String.format(" (%s:%s)", stack.fileName, stack.lineNumber)
+    @SuppressLint("LogNotTimber")
+    private fun getCallerInfo(stacks: Array<StackTraceElement>?): String {
+        if (stacks == null) {
+            return ""
         }
+        val stringBuilder = StringBuilder()
+        var i = STACK_TRACE_LEVEL
+
+        while (i <= STACK_TRACE_LEVEL + MAX_NUMBER_OF_CALLERS && i < stacks.size) {
+            val stack = stacks[i]
+
+            if (stack.className.contains(BuildConfig.APPLICATION_ID) && !stack.className.contains(LogCatTree::class.simpleName!!))
+                if (i == STACK_TRACE_LEVEL)
+                    stringBuilder.append(String.format("(%s:%s)", if (stack.fileName != null) stack.fileName else "Unknown Source", stack.lineNumber))
+                else
+                    stringBuilder.append(String.format(" , Caller: (%s:%s)", if (stack.fileName != null) stack.fileName else "Unknown Source", stack.lineNumber))
+
+            i++
+        }
+        return stringBuilder.toString()
     }
 }
